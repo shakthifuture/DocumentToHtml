51aecac8521d7e97d10f2e738ff97e445dd4f903
Sathia <smuthu@archsystemsinc.com>
2020-12-21 22:47:55 +0530

    First commit

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.util.Set;
+
+//import org.zwobble.mammoth.DocumentConverter;
+//import org.zwobble.mammoth.Result;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+//		try {
+//			DocumentConverter converter = new DocumentConverter().addStyleMap("b => em");
+//			Result<String> result = converter.convertToHtml(new File("D:\\Workspace\\New\\src\\main\\resources\\Sample.docx"));
+//			String html = result.getValue(); // The generated HTML
+//			Set<String> warnings = result.getWarnings(); // Any warnings during conversion
+//			System.out.println(html);
+//			System.out.println(warnings);
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+	}
+
+}
